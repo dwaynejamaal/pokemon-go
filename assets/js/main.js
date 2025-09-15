3438e883ed03c4179dd7b87d2e26f94f1a28ed3c
Dwayne Burns <dwayneburns@Dwaynes-iMac.lan>
2025-09-15 00:05:19 -0400

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+let variable1 = 3;
+console.log(variable1);
+
+let calc = 2 + 2;
+console.log(calc);
+console.log(calc + 4);
+
+let trainer = {
+  name: 'Dwayne',
+  age: 30,
+  pokemon: ['Pikachu', 'Charizard', 'Bulbasaur'],
+  trainer: true,
+  // Methods
+  sayName: function() {
+    console.log('My name is ' + this.name);
+  },
+}
+
+
+
